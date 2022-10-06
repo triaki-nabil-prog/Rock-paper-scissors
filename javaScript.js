97e31f962da0a935c2cf27245df57ea572667911
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-07 00:47:15 +0100

    creation of a descriptive pseudo-code
    finished turning the last part of the algorithem
    into a pseudo-code, tried to impliment as much syntax as possible
    to make the pseudo-code closer to a finished functioning code

--- javaScript.js
+++ javaScript.js
@@ -36,40 +36,89 @@ END*/
 RETURN userChocie
 END*/
 
-/*PROGRAMME COMPARE VALUES
+/*function name <playRound>(userChoice, computerChoice)
 
-    //code will compare between the values of the user and computer.
+      //code will compare between the values of the user and computer.
 
 
-    // if user choice == computer choice say "it's a tie".
+      // if user choice == computer choice say "it's a tie".
     IF (userChoice == computerChoice) THEN
-         console.log("it's a tie",'Computer played ${computerChoice} and you played ${userChoice});
+         roundWinner = "it's a TIE!";
 
 
-    // else if user choice == paper and computer choice ==rock say "you win".
+      // else if user choice == paper and computer choice ==rock say "you win".
     ELSE IF (userChoice =="paper" && computerChoice =="rock") THEN
-          console.log("You win",'Computer played ${computerChoice} and you played ${userChoice});
+          roundWinner = "you win! paper beat rock"
 
 
-    // else if user choice == papper and computer choice ==scissors say "you lose".
-    // else if user choice == rock and computer choice ==sissors say "you win".
-    // else if user choice == rock and computer choice ==paper say "you lose".
+      // else if user choice == paper and computer choice ==scissors say "you lose".
+    ELSE IF (userChoice =="paper" && computerChoice =="scissors") THEN
+          roundWinner = "you lose! scissors beat paper"
+
+
+
+      // else if user choice == rock and computer choice ==sissors say "you win".
+    ELSE IF (userChoice =="rock" && computerChoice =="scissors") THEN
+          roundWinner = "you win! rock beat scissors"
+
+
+      // else if user choice == rock and computer choice ==paper say "you lose".
+     ELSE IF (userChoice =="rock" && computerChoice =="paper") THEN
+          roundWinner = "you lose! paper beat rock"
+
+
     // else if user choice == scissors and computer choice ==rock say "you lose".
-    // else if user choice == scissors and computer choice ==papper say "you win".
+    ELSE IF (userChoice =="scissors" && computerChoice =="rock") THEN
+          roundWinner = "you lose! rock beat scissors"
 
+      // else if user choice == scissors and computer choice ==paper say "you win".
+    ELSE IF (userChoice =="scissors" && computerChoice =="paper") THEN
+          roundWinner = "you win! scissors beat paper"
+    ELSE console.log("no winner");
+    END
 
+    return roundWinner;
 END*/
 
 
 
+/* function name <game>
 
-//play a 5 rounds game loop the game 5 times then end it.
-//return the value of user score and computer score .
-// compare between the score.
-// if user score == computer score say " the game is a tie".
-// if user score < computer score say " computer won the game".
-// if user score > computer score say "you won the game".
+     //play a 5 rounds game loop the game 5 times then end it.
 
+ initial gameRound = 1;
+         userScore = 0;
+         computerScore = 0;
+
+    WHILE (gameRounds <= 5) do
+
+         computerSelection = call function getComputerChocie();
+         userSelection = call function getUserChoice();
+         Winner = call function playRound(userSelection, computerSelection);
+         console.log(`round number ${gameRounds}: ${Winner});
+         gameRounds = gameRounds + 1;
+
+         IF user wins THEN 
+            userScore++;
+         ELSE 
+            computerScore++;
+         END
+    END
+ 
+  //return the value of user score and computer score .
+  consol.log(`your score is ${userScore}`);
+  consol.log(`computer score is ${computerScore}`);
+
+  // compare between the score.
+  IF (userScore == computerScore) THEN
+      PRINT you won the game
+  ELSE IF (userScore <= computerScore) THEN
+      PRINT you lost the game
+  ELSE 
+      PRINT you won the game
+   END
+
+END*/
 
 
 
