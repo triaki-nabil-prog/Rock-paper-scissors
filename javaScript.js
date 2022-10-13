be8e392266fa06ba753315ba8295063ae77744c9
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-10-13 22:03:15 +0100

    added reset function to reset the DOM and score

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,6 @@
 // Programme name <Rock paper Scissors>
 
-let buttonSelection = document.querySelectorAll('button');
+let buttonSelection = document.querySelectorAll('.play');
 buttonSelection.forEach(button => {
     button.addEventListener('click', () => {
         playRound(button);
@@ -30,50 +30,88 @@ function getUserChoice(button) {
 }
 
 //code will compare between the values of the user and computer and select this rounds winner.
+let userScore = 0;
+let computerScore = 0;
+
+
+
+
 function playRound(button) {
     let computerChoice = getComputerChoice();
     let userChoice = getUserChoice(button);
-
     let roundWinner;
 
-    // if user choice == computer choice say "it's a tie".
     if (userChoice == computerChoice) roundWinner = "it's a tie";
-    // else if user choice == paper and computer choice ==rock say "you win".
     else if (userChoice == "paper" && computerChoice == "rock") {
         roundWinner = "you win! paper beat rock";
-
+        userScore++;
     }
-    // else if user choice == paper and computer choice ==scissors say "you lose".
     else if (userChoice == "paper" && computerChoice == "scissors") {
         roundWinner = "you lose! scissors beat paper";
-
+        computerScore++;
     }
-    // else if user choice == rock and computer choice ==sissors say "you win".
     else if (userChoice == "rock" && computerChoice == "scissors") {
         roundWinner = "you win! rock beat scissors";
-
+        userScore++;
     }
-    // else if user choice == rock and computer choice ==paper say "you lose".
     else if (userChoice == "rock" && computerChoice == "paper") {
         roundWinner = "you lose! paper beat rock";
-
+        computerScore++;
     }
-    // else if user choice == scissors and computer choice ==rock say "you lose".
     else if (userChoice == "scissors" && computerChoice == "rock") {
         roundWinner = "you lose! rock beat scissors";
-
+        computerScore++;
     }
-    // else if user choice == scissors and computer choice ==paper say "you win".
     else if (userChoice == "scissors" && computerChoice == "paper") {
         roundWinner = "you win! scissors beat paper";
-
+        userScore++;
     }
     else roundWinner = "no winner";
 
-    let div = document.querySelector('div');
-    div.textContent = `computer choice ; ${computerChoice} : ${roundWinner}`;
+    let results = document.querySelector('#results');
+    results.textContent = `computer choice ; ${computerChoice} : ${roundWinner}`;
+
+    let userScoreDisplay = document.querySelector('#userScoreDisplay');
+    userScoreDisplay.textContent = `user Score; ${userScore}`;
+
+    let computerScoreDisplay = document.querySelector('#computerScoreDisplay');
+    computerScoreDisplay.textContent = `computer Score;${computerScore} `;
+
+    let winner = document.querySelector('#winner');
+
+    if (userScore == 5) {
+        winner.textContent = "THE USER";
+        
+    }
+    else if (computerScore == 5) {
+        winner.textContent = "THE COMPUTER";
+        
+    }
+    else if(computerScore > 5 || userScore > 5)resetGame();
+    else {
+        winner.textContent = "the Winner is !";
+    }
+
+
     return;
 }
 
+let resetg = document.querySelector('#reset');
+resetg.addEventListener('click', resetGame);
 
+function resetGame(){
+    computerScore = 0;
+    userScore = 0;
+
+    let results =document.querySelector('#results');
+    results.textContent = "results";
+    winner.textContent = "the Winner is !";
+    let userScoreDisplay = document.querySelector('#userScoreDisplay');
+    userScoreDisplay.textContent = `user Score;`;
+
+    let computerScoreDisplay = document.querySelector('#computerScoreDisplay');
+    computerScoreDisplay.textContent = `computer Score;`;
+
+    return;
+}
 
